694b1fffc9747b23a99adddbff8ba99a38f9fc17
Mona Waseem <monaqwaseem@gmail.com>
2021-05-27 19:49:49 +0000

    Edited code with correct output

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,3 @@
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-
-};
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
@@ -15,3 +7,12 @@ const eqArrays = function (arr1, arr2) {
   }
   return true;
 }
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+
+};
+
