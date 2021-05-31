be47381a1d2ad2a33270a675ee9d2830f26f4797
Mona Waseem <monaqwaseem@gmail.com>
2021-05-31 17:24:34 +0000

    Create modules for test files.

---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+// TEST CODE
+assertEqual("Hello World", "Hello World")
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
